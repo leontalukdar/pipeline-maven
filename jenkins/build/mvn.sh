633e1efc4ef9ab7669b4c12c1937620bab050e69
wahidul islam <wahidul.islam@bjitgroup.com>
2022-09-27 12:54:10 +0600

    Implementations: - Update workspace

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,4 +4,5 @@ echo "*********************"
 echo "**** Building Jar ***"
 echo "*********************"
 
-docker run -v $PWD/java-app:/app -v $PWD/root/.m2:/root/.m2 -w /app maven:3.5.4-alpine "$@"
+WORKSPACE=/home/wahidul/Projects/jenkins/jenkins-docker/jenkins_home/workspace/pipeline-docker-maven
+docker run -v $WORKSPACE/java-app:/app -v $PWD/root/.m2:/root/.m2 -w /app maven:3.5.4-alpine "$@"
