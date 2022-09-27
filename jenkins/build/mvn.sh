8fdff0ff6b1ec8a6bdb444f6ab33bf5094520d42
wahidul islam <wahidul.islam@bjitgroup.com>
2022-09-27 13:06:02 +0600

    Add credentials

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -5,4 +5,4 @@ echo "**** Building Jar ***"
 echo "*********************"
 
 WORKSPACE=/home/wahidul/Projects/jenkins/jenkins-docker/jenkins_home/workspace/pipeline-docker-maven
-docker run -v $WORKSPACE/java-app:/app -v $PWD/root/.m2:/root/.m2 -w /app maven:3.5.4-alpine "$@"
+docker run -v $WORKSPACE/java-app:/app -v $WORKSPACE/root/.m2:/root/.m2 -w /app maven:3.5.4-alpine "$@"
