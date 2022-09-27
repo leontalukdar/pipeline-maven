25d795ce2cbe6fa18359cd8564f02a88cd2bb78c
wahidul islam <wahidul.islam@bjitgroup.com>
2022-09-27 14:24:25 +0600

    manual build tag

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -6,4 +6,4 @@ echo "***************************************"
 echo "******* Building Docker Image *********"
 echo "***************************************"
 
-cd jenkins/build/ && docker-compose -f docker-compose-build.yml build
+cd jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache
